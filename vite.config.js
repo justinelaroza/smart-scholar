623366f2c656233877ce87ca6cbe1e04c8c6df55
justinelaroza <larozajustine15@gmail.com>
2025-11-06 12:15:08 +0800

    reconfigured vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/auth/login.js',
+                'resources/js/auth/register.js',
+                'resources/js/auth/forgot-pass.js',
+                'resources/js/pages/home.js',
+                'resources/js/pages/profile.js',
+                'resources/js/pages/scholarship.js',
+                'resources/js/pages/support.js',
+                'resources/js/pages/upload.js',
+                'resources/js/components/navbar.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
-});
+})
