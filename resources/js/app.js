fa39cd9d9950732e59211702b5426c9a0c3b8c48
justinelaroza <larozajustine15@gmail.com>
2025-11-07 20:52:50 +0800

    fixing datepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,4 @@ window.Swal = Swal;
 flatpickr(".datepicker", {
   altInput: true,
   dateFormat: "Y-m-d",
-  onReady: function(selectedDates, dateStr, instance) {
-    if (instance.altInput) {
-      instance.altInput.removeAttribute("readonly");
-      instance.altInput.setAttribute("placeholder", "Birthday");
-    }
-  }
 });
