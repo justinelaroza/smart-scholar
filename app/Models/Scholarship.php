945f964946c0bf53dd90b8de9964614e85c99a20
justinelaroza <larozajustine15@gmail.com>
2025-11-16 22:46:28 +0800

    try

--- app/Models/Scholarship.php
+++ app/Models/Scholarship.php
@@ -6,6 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Scholarship extends Model
 {
+  protected $fillable = [
+        'title', 'funder', 'description', 'education_level', 
+        'application_start', 'submission_deadline', 'amount', 
+        'status', 'image', 'residency_requirement'
+    ];
+
+    // Add this accessor to handle binary image data
+    public function getImageBase64Attribute()
+    {
+        if (!$this->image) {
+            return null;
+        }
+        
+        $imageData = $this->image;
+        
+        // Handle PostgreSQL resource stream
+        if (is_resource($imageData)) {
+            rewind($imageData);
+            $imageData = stream_get_contents($imageData);
+        }
+        
+        return base64_encode($imageData);
+    }
+    
   public function generalInfos()
   {
       return $this->hasMany(GeneralInfo::class);
