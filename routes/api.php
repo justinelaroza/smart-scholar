fb8a0b22dbd2e024ee5a07f6170380348c02733c
justinelaroza <larozajustine15@gmail.com>
2025-11-20 11:43:01 +0800

    added sanctum again

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FileUploadApiController;
 
 
-
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/file-uploads/{id}/document/{field}/download', [FileUploadApiController::class, 'downloadDocument']);
     Route::get('/file-uploads/{id}/document/{field}/view', [FileUploadApiController::class, 'viewDocument']);
+});
