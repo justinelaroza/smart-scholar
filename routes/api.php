0335481021e9d924293563bdd780dc4b72e443e5
justinelaroza <larozajustine15@gmail.com>
2025-11-29 22:50:32 +0800

    temporarly deleted sanctum to check

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,7 @@ use App\Http\Controllers\Api\BroadcastController;
 
 Route::get('/cron/update-scholarships', [UpdateScholarshipStatusController::class, 'updateScholarshipStatus']);
 
-Route::middleware('auth:sanctum')->group(function () {
+
     Route::post('/broadcast/scholarship-created', [BroadcastController::class, 'scholarshipCreated']);
     Route::post('/broadcast/scholarship-updated', [BroadcastController::class, 'scholarshipUpdated']);
     Route::post('/broadcast/scholarship-deleted', [BroadcastController::class, 'scholarshipDeleted']);
-});;
